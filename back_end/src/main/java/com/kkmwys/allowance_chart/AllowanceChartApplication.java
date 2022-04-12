51a2c026cf8b8408d4bfa9b2f5dd5feb4e4e7fe6
Apisapple <38449976+Apisapple@users.noreply.github.com>
2022-04-12 23:01:38 +0900

    init backend project
    백엔드 프로젝트 초기화

---
+++ back_end/src/main/java/com/kkmwys/allowance_chart/AllowanceChartApplication.java
@@ -0,0 +1,13 @@
+package com.kkmwys.allowance_chart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllowanceChartApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AllowanceChartApplication.class, args);
+  }
+
+}
---
+++ back_end/src/test/java/com/kkmwys/allowance_chart/AllowanceChartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkmwys.allowance_chart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllowanceChartApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
