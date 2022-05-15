c4ccd7b36a7834b5023de3a8b43f58b6f4d7c1c3
Apisapple <38449976+Apisapple@users.noreply.github.com>
2022-05-16 02:41:14 +0900

    Update IncomeView.vue
    vue api 요청 추가

--- front_end/client/src/views/IncomeView.vue
+++ front_end/client/src/views/IncomeView.vue
@@ -1,11 +1,15 @@
 /* eslint-disable indent */
 <template>
     <div class="income">
-        <table class="table table-striped">
+        <table class="table table-striped" id="tableComponent">
           <thead>
               <tr>
                 <th scope="col">TEST</th>
                 <th scope="col">QWER</th>
+                <th v-for='field in fields' :key='field' @click="sortTable(field)">
+                  {{field}}
+                  <i class="bi bi-sort-alpha-down" aria-label="Sort Icon" ></i>
+                </th>
               </tr>
           </thead>
           <tbody>
@@ -19,7 +23,10 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+  name: 'TableComponent',
   data() {
     return {
       fields: ['first_name', 'last_name', 'age'],
@@ -27,6 +34,17 @@ export default {
         { age: 40, first_name: 'name' },
       ],
     };
+  },
+  created() {
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const vm = this;
+
+    axios.get('http://localhost:8080').then((response) => {
+      console.log(response);
+      vm.items = response.data;
+    }).catch((error) => {
+      console.log(error);
+    });
   },
 };
 </script>
