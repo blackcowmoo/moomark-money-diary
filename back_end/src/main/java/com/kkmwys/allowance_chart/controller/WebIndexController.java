d451e24a834952ecb54ae067c357d7294fa7190a
Apisapple <38449976+Apisapple@users.noreply.github.com>
2022-06-29 01:34:38 +0900

    category 정보 화면 출력
    category 정보 화면 출력

--- back_end/src/main/java/com/kkmwys/allowance_chart/controller/WebIndexController.java
+++ back_end/src/main/java/com/kkmwys/allowance_chart/controller/WebIndexController.java
@@ -1,10 +1,19 @@
 package com.kkmwys.allowance_chart.controller;
 
+import com.kkmwys.allowance_chart.data.dto.CategoryDto;
+import com.kkmwys.allowance_chart.domain.Category;
+import com.kkmwys.allowance_chart.service.CategoryService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class WebIndexController {
+
+  private CategoryService categoryService;
 
   @GetMapping("/")
   public String getRootPage() {
@@ -27,7 +36,12 @@ public class WebIndexController {
   }
 
   @GetMapping("/config")
-  public String getSettingPage() {
+  public String getSettingPage(Model model) {
+    List<CategoryDto> incomeCategoryList =  categoryService.getCategoryListByType("income");
+    List<CategoryDto> spendingCategoryList = categoryService.getCategoryListByType("spending");
+
+    model.addAttribute("incomeCategoryList", incomeCategoryList);
+    model.addAttribute("spendingCategoryList", spendingCategoryList);
     return "/config";
   }
 
