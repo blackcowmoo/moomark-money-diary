197a7c1c3a8195a111d0d0b0a625b67247ff6d64
Apisapple <38449976+Apisapple@users.noreply.github.com>
2022-08-28 00:20:12 +0900

    Update ChartDataRepository.java
    [FIX] 조회가 되지 않던 버그 수정

--- back_end/src/main/java/com/kkmwys/allowance_chart/repository/ChartDataRepository.java
+++ back_end/src/main/java/com/kkmwys/allowance_chart/repository/ChartDataRepository.java
@@ -4,8 +4,11 @@ import com.kkmwys.allowance_chart.domain.Category;
 import com.kkmwys.allowance_chart.domain.ChartData;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ChartDataRepository extends JpaRepository<ChartData, Long> {
   List<ChartData> findChartDataByCategory(Category category);
+
+  @Query(value = "select d from ChartData d JOIN d.category c WHERE c.name LIKE %?1%")
   List<ChartData> findChartDataByCategoryType(String type);
 }
