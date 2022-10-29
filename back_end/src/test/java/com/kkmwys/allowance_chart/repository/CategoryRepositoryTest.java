124feb70f784550056ec8ead03d96c2ebb14ab59
Apisapple <38449976+Apisapple@users.noreply.github.com>
2022-10-29 09:07:17 +0900

    Add repository test
    repository 테스트 코드 추가

---
+++ back_end/src/test/java/com/kkmwys/allowance_chart/repository/CategoryRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.kkmwys.allowance_chart.repository;
+
+import com.kkmwys.allowance_chart.domain.Category;
+import com.kkmwys.allowance_chart.domain.CategoryType;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@DataJpaTest(showSql = true)
+@ExtendWith(SpringExtension.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CategoryRepositoryTest {
+
+  @Autowired
+  private CategoryRepository categoryRepository;
+
+
+  @Test
+  @DisplayName("Category 정보 저장 테스트")
+  void saveCategory() {
+    Category category = Category.builder()
+        .id(1L)
+        .type(CategoryType.INCOME)
+        .name("salary")
+        .build();
+
+    categoryRepository.save(category);
+    Category savedData = categoryRepository.findById(1L).orElse(new Category());
+    Assertions.assertThat(category.getId()).isEqualTo(savedData.getId());
+    Assertions.assertThat(category.getName()).isEqualTo(savedData.getName());
+    Assertions.assertThat(category.getType()).isEqualTo(savedData.getType());
+  }
+
+  @Test
+  @DisplayName("Category 정보 조회 함수")
+  void getCategoryById() {
+
+  }
+}
---
+++ back_end/src/test/java/com/kkmwys/allowance_chart/repository/ChartDataRepositoryTest.java
@@ -0,0 +1,7 @@
+package com.kkmwys.allowance_chart.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChartDataRepositoryTest {
+
+}
