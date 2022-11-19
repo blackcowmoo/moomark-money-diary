88964da6f1c02f42e736213325e1e403ee28e327
Apisapple <kimminyong2034@gmail.com>
2022-11-20 03:12:10 +0900

    Update ChartDataRepositoryTest.java
    init ChartDataRepositoryTest

--- back_end/src/test/java/com/kkmwys/allowance_chart/repository/ChartDataRepositoryTest.java
+++ back_end/src/test/java/com/kkmwys/allowance_chart/repository/ChartDataRepositoryTest.java
@@ -2,6 +2,35 @@ package com.kkmwys.allowance_chart.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import groovy.util.logging.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@DataJpaTest(showSql = true)
+@ExtendWith(SpringExtension.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Slf4j
 class ChartDataRepositoryTest {
+
+  @Autowired
+  private ChartDataRepository chartDataRepository;
+
+  @Autowired
+  private CategoryRepository categoryRepository;
+
+  @BeforeEach
+  void initData() {
+
+  }
+
+  @Test
+  void saveChartDataTest() {
+
+  }
 
 }
